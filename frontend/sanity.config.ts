230fa504f82c86e0438360936d4138f7a9361da1
buster <dioecisms_coralline@aleeas.com>
2026-07-08 18:53:59 +0530

    changes

---
+++ frontend/sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+
+export default defineConfig({
+  name: "default",
+  title: "My Studio",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  plugins: [structureTool()],
+  schema: {
+    types: [],
+  },
+});
